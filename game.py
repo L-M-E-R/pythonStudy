ea1b735e70cf9c82d6db1194ee928f7ad515d5a7
L-M-E-R <1806393533@qq.com>
2022-03-18 11:42:15 +0800

    Changes

---
+++ game.py
@@ -0,0 +1,8 @@
+temp = input("guess which number i think")
+guess = int(temp)
+
+if guess == 8:
+    print("yes")
+else:
+    print("no")
+
---
+++ string.py
@@ -0,0 +1,5 @@
+import random
+strTmp = "qwertyuiop"
+print(strTmp[0:5:2])
+
+
